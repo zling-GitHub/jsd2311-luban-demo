dfbe367b5e1a901ee156b063489ac96c9298ba5e
肖旭伟 <xiaoxw@tedu.cn>
2024-04-07 17:30:32 +0800

    JSD2311-DAY01-PM 04 订单模块拆分完毕

---
+++ luban-demo-order/luban-demo-order-main/src/main/java/com/tarena/demo/luban/all/main/service/OrderService.java
@@ -0,0 +1,7 @@
+package com.tarena.demo.luban.all.main.service;
+
+import com.tarena.demo.luban.protocol.order.param.OrderAddParam;
+
+public interface OrderService {
+    void addOrder(OrderAddParam param);
+}
---
+++ luban-demo-order/luban-demo-order-main/src/main/java/com/tarena/demo/luban/all/main/service/impl/OrderServiceImpl.java
@@ -0,0 +1,34 @@
+package com.tarena.demo.luban.all.main.service.impl;
+
+import com.tarena.demo.luban.all.main.mapper.OrderMapper;
+import com.tarena.demo.luban.all.main.service.OrderService;
+import com.tarena.demo.luban.protocol.cart.param.CartDeleteParam;
+import com.tarena.demo.luban.protocol.order.dos.OrderDO;
+import com.tarena.demo.luban.protocol.order.param.OrderAddParam;
+import com.tarena.demo.luban.protocol.stock.param.StockReduceCountParam;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class OrderServiceImpl implements OrderService {
+    @Autowired
+    private OrderMapper orderMapper;
+    @Override public void addOrder(OrderAddParam param) {
+        // 减库存
+        StockReduceCountParam stockReduceCountParam=new StockReduceCountParam();
+        stockReduceCountParam.setReduceCount(param.getCount());
+        stockReduceCountParam.setProductCode(param.getProductCode());
+        stockReduceCountParam.setOrderSn(param.getOrderSn());
+        //stockService.reduceCommodityCount(stockReduceCountParam);
+        //增订单
+        OrderDO orderDO=new OrderDO();
+        BeanUtils.copyProperties(param,orderDO);
+        orderMapper.insertOrder(orderDO);
+        //删除购物车
+        CartDeleteParam cartDeleteParam=new CartDeleteParam();
+        cartDeleteParam.setUserId(param.getUserId());
+        cartDeleteParam.setProductCode(param.getProductCode());
+        //cartService.deleteCart(cartDeleteParam);
+    }
+}
