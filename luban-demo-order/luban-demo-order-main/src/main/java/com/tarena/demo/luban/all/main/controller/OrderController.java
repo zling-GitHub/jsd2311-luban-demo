69e35fdede4108786c3b6f22cfa7034f18242244
肖旭伟 <83123095@qq.com>
2024-04-09 10:41:49 +0800

    JSD2311-DAY03-AM 02 bootstrap的正确格式

--- luban-demo-order/luban-demo-order-main/src/main/java/com/tarena/demo/luban/all/main/controller/OrderController.java
+++ luban-demo-order/luban-demo-order-main/src/main/java/com/tarena/demo/luban/all/main/controller/OrderController.java
@@ -1,6 +1,5 @@
 package com.tarena.demo.luban.all.main.controller;
 
-
 import com.tarena.demo.luban.all.main.service.OrderService;
 import com.tarena.demo.luban.commons.restful.JsonResult;
 import com.tarena.demo.luban.protocol.order.param.OrderAddParam;
@@ -9,6 +8,7 @@ import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/base/order")
 @Api(tags = "订单")
 public class OrderController {
+
     @Autowired
     private OrderService orderService;
     @PostMapping("/add")
