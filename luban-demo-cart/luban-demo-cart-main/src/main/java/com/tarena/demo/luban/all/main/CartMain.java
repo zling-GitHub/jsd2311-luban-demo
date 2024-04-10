42824ccb607a4935cd51133c8bcc2e70811be441
肖旭伟 <83123095@qq.com>
2024-04-10 11:54:51 +0800

    JSD2311-DAY04-AM 06 springboot导入dubbo.xml

--- luban-demo-cart/luban-demo-cart-main/src/main/java/com/tarena/demo/luban/all/main/CartMain.java
+++ luban-demo-cart/luban-demo-cart-main/src/main/java/com/tarena/demo/luban/all/main/CartMain.java
@@ -6,12 +6,15 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ImportResource;
 
 import java.io.ObjectInputStream;
 import java.net.ServerSocket;
 
 @SpringBootApplication
 @MapperScan("com.tarena.demo.luban.all.main.mapper")
+//额外导入xml配置资源
+@ImportResource("dubbo.xml")
 public class CartMain {
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(CartMain.class, args);
