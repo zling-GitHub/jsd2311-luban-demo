e8630f680b2b2d41c3eb495f644f3d2e6cd3c13f
肖旭伟 <83123095@qq.com>
2024-04-11 11:20:15 +0800

    JSD2311-DAY05-AM 07 @PropertySource 导入jdbc.properties

--- luban-spring-boot-starter/src/test/java/cn/tedu/test/luban/demo/bean/Bean01.java
+++ luban-spring-boot-starter/src/test/java/cn/tedu/test/luban/demo/bean/Bean01.java
@@ -1,10 +1,14 @@
 package cn.tedu.test.luban.demo.bean;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public class Bean01 {
+    @Value("${luban.user.name}")
+    private String username;
     public Bean01() {
         System.out.println("当前bean01被容器加载了");
     }
     public void sayHi(String name){
-        System.out.println("bean01对["+name+"]说你好");
+        System.out.println("bean01对["+name+"]说你好"+"我是:"+username);
     }
 }
--- luban-spring-boot-starter/src/test/java/cn/tedu/test/luban/demo/config/MyConfiguration01.java
+++ luban-spring-boot-starter/src/test/java/cn/tedu/test/luban/demo/config/MyConfiguration01.java
@@ -3,15 +3,13 @@ package cn.tedu.test.luban.demo.config;
 import cn.tedu.test.luban.demo.bean.Bean01;
 import cn.tedu.test.luban.demo.selector.MySelector;
 import com.example.config.MyConfiguration02;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.*;
 
 @Configuration
 @ComponentScan(basePackages = {"cn.tedu.test.luban.demo.bean"})
 //@Import(value={MyConfiguration02.class})
 @Import(value={MySelector.class})
+@PropertySource("jdbc.properties")
 public class MyConfiguration01 {
     public MyConfiguration01() {
         System.out.println("当前config01被容器加载了");
