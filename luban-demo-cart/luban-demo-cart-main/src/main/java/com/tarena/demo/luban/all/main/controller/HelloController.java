5286b41e6bf0d443e458181f0aa7abd0f9d3a58c
肖旭伟 <83123095@qq.com>
2024-04-12 14:23:23 +0800

    JSD2311-DAY06-PM 04 cart购物车准备测试接口/hello

---
+++ luban-demo-cart/luban-demo-cart-main/src/main/java/com/tarena/demo/luban/all/main/controller/HelloController.java
@@ -0,0 +1,15 @@
+package com.tarena.demo.luban.all.main.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @Value("${server.port}")
+    private String port;
+    @GetMapping("/hello")
+    public String sayHi(String name){
+        return "hello "+name+",I am from ["+port+"]";
+    }
+}
