462a9ce0afed9b5a47ba56b3238c415e1b3d6d7b
he-xiaoluo <zling306@163.com>
2024-04-16 16:30:11 +0800

    启动类设置并发限流参数

--- luban-demo-sentinel/sentinel-demo01/src/main/java/com/sentinel/demo01/SentinelDemo01Main.java
+++ luban-demo-sentinel/sentinel-demo01/src/main/java/com/sentinel/demo01/SentinelDemo01Main.java
@@ -1,11 +1,28 @@
 package com.sentinel.demo01;
 
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
+import com.alibaba.csp.sentinel.slots.block.flow.FlowRuleManager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class SentinelDemo01Main {
     public static void main(String[] args) {
         SpringApplication.run(SentinelDemo01Main.class, args);
+        // 设置一个FlowRule，限制每秒钟最多10个请求，超过10个请求则触发熔断
+        List<FlowRule> rules = new ArrayList<>();
+        FlowRule rule = new FlowRule();
+        rule.setResource("sayHi");
+        //设置限流级别
+        rule.setGrade(1);
+        // 设置上限阈值
+        rule.setCount(10);
+        // 设置每秒钟的请求数
+        rules.add(rule);
+        // 加载规则
+        FlowRuleManager.loadRules(rules);
     }
 }
--- luban-demo-sentinel/sentinel-demo01/src/main/java/com/sentinel/demo01/controller/HelloController.java
+++ luban-demo-sentinel/sentinel-demo01/src/main/java/com/sentinel/demo01/controller/HelloController.java
@@ -1,11 +1,16 @@
 package com.sentinel.demo01.controller;
 
+import com.alibaba.csp.sentinel.Entry;
+import com.alibaba.csp.sentinel.SphU;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import com.sentinel.demo01.service.HelloService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 public class HelloController {
 
@@ -13,8 +18,19 @@ public class HelloController {
     private HelloService helloService;
 
     @GetMapping("/hello")
-    public String sayHi(String name){
-        String result = helloService.sayHi(name);
+    public String sayHi(String name) {
+        Entry entry = null;
+        String result = null;
+        try {
+            entry = SphU.entry("sayHi");
+            result = helloService.sayHi(name);
+        } catch (BlockException e) {
+            log.error("sentinel限制访问了:", e);
+            return "sorry";
+        } finally {
+            if (entry != null)
+                entry.exit();
+        }
         return result;
     }
 
